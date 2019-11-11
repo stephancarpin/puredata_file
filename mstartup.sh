3961199d839a83309b38309a8ecfffdf1198b060
stephan <machinekit@beaglebone.localdomain>
2019-11-11 13:44:04 +0000

    from bebb

---
+++ mstartup.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+# Print the IP address
+echo "\ngood:)\m" > ./puredata_file/loggg.txt
+#echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+#/sbin/route add default gw 192.168.6.1
+
+#usinf=g alsa
+#pd -alsa -rt -nogui  -r 44100 -blocksize 64 -audiobuf 35 -audioadddev AudioBox ./puredata_file/adcTest.pd
+#pd -alsa -rt -nogui -r 44100 -blocksize 64 -audiobuf 35 -audioadddev AudioBox ./puredata_file/BBmixerLooperUndo.pd
+#using 0ss
+#sleep 10
+#sudo systemctl disable avahi-daemon.socket
+#sudo service avahi-daemon stop
+sudo modprobe snd-pcm-oss
+sleep 10
+#pd -oss -nrt -nogui  -r 44100 -blocksize 32 -audiobuf 10 ./puredata_file/BBmixerLooperUndo.pd
+#pd -oss -nrt -nogui  -r 44100 -blocksize 8 -audiobuf 10 ./puredata_file/BBmixerLooperUndo.pd
